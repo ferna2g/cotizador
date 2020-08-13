fdcc031fa5534060376e24bc6b007bf3f488d7b5
fer g <arielgeronimo517@gmail.com>
2020-08-12 21:28:01 -0600

    Creando el componente Formulario

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from '@emotion/styled'
 import imagen from './cryptomonedas.png'
+import Formulario from './components/Formulario'
 
 const Contenedor = styled.div`
   max-width: 900px;
@@ -16,6 +17,23 @@ const Imagen = styled.img`
   max-width: 100%;
   margin-top: 5rem;
 `
+const Heading = styled.h1`
+  font-family: 'Bebas Neue', cursive;
+  color: #FFF;
+  text-align: left;
+  font-weight: 700;
+  font-size: 50px;
+  margin-bottom: 50px;
+  margin-top: 80px;
+
+  &::after{
+    content '';
+    width: 100px;
+    height: 6px;
+    background-color: #66A2FE;
+    display: block;
+  }
+`
 
 function App() {
   return (
@@ -27,7 +45,10 @@ function App() {
           />
       </div>
       <div>
-
+        <Heading>
+          Cotiza Criptomonedas
+        </Heading>
+        <Formulario />
       </div>
     </Contenedor>
   );
