519536c6178fefd2822473807f9cf9cb7a2856bd
fer g <arielgeronimo517@gmail.com>
2020-08-16 19:09:20 -0600

    pasando criptomonedas al custom hook desde una API

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,7 +1,8 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import styled from "@emotion/styled"
 import useMoneda from '../hooks/useMoneda'
 import useCriptomoneda from '../hooks/useCriptomoneda'
+import axios from 'axios'
 
 const Boton = styled.input`
   margin-top: 20px;
@@ -23,6 +24,8 @@ const Boton = styled.input`
 
 const Formulario = () => {
 
+  const [listacripto, guardarCriptomoneda] = useState([])
+
   const MONEDAS = [
     {codigo:'USD', nombre:'Dolar Estadounidense'},
     {codigo:'MXN', nombre:'Peso Mexicano'},
@@ -34,7 +37,19 @@ const Formulario = () => {
   const [ moneda, SelectMonedas] = useMoneda('Elige tu moneda', '', MONEDAS)
 
 //utilizamos criptomoneda
-  const [ criptomoneda, SelectCripto ] = useCriptomoneda('Elige tu criptomoneda', '', )
+  const [ criptomoneda, SelectCripto ] = useCriptomoneda('Elige tu criptomoneda', '', listacripto )
+
+  //consumimos la api
+  useEffect(() => {
+    const consultarAPI  = async () => {
+      const url = 'https://min-api.cryptocompare.com/data/top/mktcapfull?limit=10&tsym=USD'
+      const resultado = await axios.get(url)
+
+      guardarCriptomoneda(resultado.data.Data)
+    }
+
+    consultarAPI()
+  }, [])
 
   return(
     <form>
