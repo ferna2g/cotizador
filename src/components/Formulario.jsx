8c528146275b9ffdf757df9568a527f9dbd083e2
fer g <arielgeronimo517@gmail.com>
2020-08-16 18:51:03 -0600

    Creando un segundo custom hook

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,6 +1,7 @@
 import React from "react"
 import styled from "@emotion/styled"
 import useMoneda from '../hooks/useMoneda'
+import useCriptomoneda from '../hooks/useCriptomoneda'
 
 const Boton = styled.input`
   margin-top: 20px;
@@ -32,10 +33,14 @@ const Formulario = () => {
   //utilizar useMoneda
   const [ moneda, SelectMonedas] = useMoneda('Elige tu moneda', '', MONEDAS)
 
+//utilizamos criptomoneda
+  const [ criptomoneda, SelectCripto ] = useCriptomoneda('Elige tu criptomoneda', '', )
+
   return(
     <form>
 
     <SelectMonedas />
+    <SelectCripto />
       <Boton
         type="submit"
         value="Calcular"
